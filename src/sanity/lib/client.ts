8a81701069325f59659ae947348cb597d75b25bb
Rhushriaj Patil <rishanimates@yahoo.com>
2026-06-10 21:50:37 +0200

    chore: set default Sanity project ID for development environment

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '2vzs8piy',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: true, // Set to false if you need real-time data
